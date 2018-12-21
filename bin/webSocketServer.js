8f03dc0b74f21cfd0d42332b34207b7ce57a0bd3
littlepeng <littlepeng@futunn.com>
2018-12-21 21:31:05 +0800

    聊天玩玩

---
+++ bin/webSocketServer.js
@@ -0,0 +1,26 @@
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: 8080});
+
+// 连接池
+var clients = [];
+
+wss.on('connection', function(ws) {
+    // 将该连接加入连接池
+    clients.push(ws);
+    console.log('wlian jie ....');
+    ws.on('message', function(message) {
+        // 广播消息
+        clients.forEach(function(ws1){
+            if(ws1 !== ws) {
+                ws1.send(message);
+            }
+       })
+    });
+
+    ws.on('close', function(message) {
+        // 连接关闭时，将其移出连接池
+        clients = clients.filter(function(ws1){
+            return ws1 !== ws
+        })
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', {title:{chainese:'小鹏的个人网站',english:'Welcome to littlepeng\'s Personal Website'}});
 });
-
+router.get('/chart', function(req, res, next){
+	res.render('chart', {title: ''});
+});
 module.exports = router;
